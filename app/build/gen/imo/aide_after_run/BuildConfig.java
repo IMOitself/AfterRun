b8e8ca37fa2a4f3cd725d30d10c56028f114c48e
IMOitself <russjonn@gmail.com>
2025-06-10 14:16:55 +0800

    freshly bake goods

---
+++ app/build/gen/imo/aide_after_run/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package imo.aide_after_run;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ app/src/main/java/imo/aide_after_run/MainActivity.java
@@ -0,0 +1,14 @@
+package imo.aide_after_run;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
